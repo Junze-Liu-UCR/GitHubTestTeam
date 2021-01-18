660ce3fb3f230de38b179c6d9c47cfd7f81a23c3
ONI-WORKSTATION-01 <ONI-WORKSTATION-01@DESKTOP-4G29K6O>
2021-01-18 00:11:32 -0800

    Account B edited

--- GitHubTestTeam/GitHubTestTeam.cpp
+++ GitHubTestTeam/GitHubTestTeam.cpp
@@ -2,9 +2,14 @@
 
 using namespace std; 
 
+int accountBBranch() {
+	cout << "B editing" << endl; 
+	return 0; 
+}
 int main() {
 
 	cout << "repository published to organization" << endl; 
+	accountBBranch(); 
 
 	return 0; 
 }
